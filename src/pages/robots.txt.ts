7f50ca1f072152698c7db083d745713b4077a899
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2024-09-28 21:30:07 -0400

    why was this here

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -50,5 +50,4 @@ Sitemap: ${new URL("sitemap-index.xml", SITE.website).href}
 export const GET: APIRoute = () =>
   new Response(robots, {
     headers: { "Content-Type": "text/plain" },
-    status: 418,
   });
