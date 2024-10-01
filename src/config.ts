4882c1653f1638395397ac9899cf33fdabd92909
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2024-10-01 12:36:36 -0400

    remove redundant property

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,6 @@ export const SITE: Site = {
   profile: "https://kaascevich.github.io/",
   desc: "my tiny little haven on the internet",
   title: "kaascevich",
-  lightAndDarkMode: true,
   postPerIndex: 5,
   postPerPage: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
