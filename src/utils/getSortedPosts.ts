fe590ad90fa524abd7e218fcddc39191e18c4b78
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2024-10-08 08:48:17 -0400

    factor out post sorting

--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,16 +1,11 @@
 import type { CollectionEntry } from "astro:content";
 import postFilter from "@utils/postFilter";
 
+const getPostSortIndex = ({ data }: CollectionEntry<"blog">) =>
+  Math.floor(new Date(data.modified ?? data.published).getTime() / 1000);
+
 const getSortedPosts = (posts: CollectionEntry<"blog">[]) => posts
   .filter(postFilter)
-  .sort(
-    (a, b) =>
-      Math.floor(
-        new Date(b.data.modified ?? b.data.published).getTime() / 1000
-      ) -
-      Math.floor(
-        new Date(a.data.modified ?? a.data.published).getTime() / 1000
-      )
-  );
+  .sort((a, b) => getPostSortIndex(b) - getPostSortIndex(a));
 
 export default getSortedPosts;
